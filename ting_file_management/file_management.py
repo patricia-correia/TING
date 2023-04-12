299436ccbc7cbc0153f9995db2909f1d6e03c11d
Patricia Correia <pncorreia25@gmail.com>
2023-04-12 11:35:54 -0300

    req 02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        return sys.stderr.write('Formato inválido')
+
+    try:
+        with open(path_file) as path:
+            return path.read().split('\n')
+
+    except FileNotFoundError:
+        return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
