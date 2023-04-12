00ddd76b389e9ae40d8b5d9c4ebbf3ad4cfde054
Patricia Correia <pncorreia25@gmail.com>
2023-04-12 13:47:10 -0300

    req 03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+
+    output = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(file),
+        'linhas_do_arquivo': file
+    }
+
+    for index in range(len(instance)):
+        if instance.search(index)['nome_do_arquivo'] == path_file:
+            return None
+
+    instance.enqueue(output)
+    return sys.stdout.write(str(output))
 
 
 def remove(instance):
