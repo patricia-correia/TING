f7af5b6a62f4c72c0f7fdbb0d8a1f30d0f8a641f
Patricia Correia <pncorreia25@gmail.com>
2023-04-12 15:07:24 -0300

    req 05

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -29,4 +29,9 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        response = instance.search(position)
+        return sys.stdout.write(str(response))
+
+    except IndexError:
+        return sys.stderr.write('Posição inválida')
