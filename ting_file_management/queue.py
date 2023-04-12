b9d2b3fae4f38a3ad0d26211397ca96780e2a824
Patricia Correia <pncorreia25@gmail.com>
2023-04-12 11:07:43 -0300

    req 01

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,25 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._list = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._list)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._list.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if len(self._list) == 0:
+            return None
+
+        return self._list.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        max_index = len(self._list)
+        if index < 0 or index >= max_index:
+            raise IndexError('Índice Inválido ou Inexistente')
+
+        value = self._list[index]
+
+        return value
