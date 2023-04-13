afe955151fb0c6d3f893593f4cd94830df40f4e0
Patricia Correia <pncorreia25@gmail.com>
2023-04-13 09:40:30 -0300

    req 07.1

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    found = []
+
+    for i in range(len(instance)):
+        data = instance.search(i)
+        path = data["linhas_do_arquivo"]
+        occurrences = []
+        for file in range(len(path)):
+            if word.lower() in path[file].lower():
+                occurrences.append(file + 1)
+
+        if len(occurrences):
+            found.append({
+                'palavra': word,
+                'arquivo': data['nome_do_arquivo'],
+                'ocorrencias': [{'linha': line} for line in occurrences]
+            })
+
+    return found
 
 
 def search_by_word(word, instance):
